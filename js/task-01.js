ed11b47bfeb82d74d656924cf8762538256c6a65
SebastianPrf <sebopar@gmail.com>
2023-01-11 22:00:58 +0100

    a

---
+++ js/task-01.js
@@ -0,0 +1,5 @@
+let categoriesCount = document.querySelectorAll(`.item`);
+console.log(`Number of categories: ${categoriesCount.length}`);
+let categoryElementsCount = categoriesCount.forEach((e) => { console.log(`Category \nwow`) })
+
+
